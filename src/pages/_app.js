490ba5cd239c996ff187369455f741cdb47e81cd
Paige Caskey <paigecaskey@gmail.com>
2024-06-25 03:35:38 -0700

    add name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/global.css';
+import Head from 'next/head'; // Import Head from Next.js
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Paige Caskey</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
+
